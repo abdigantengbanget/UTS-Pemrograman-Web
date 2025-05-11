7608e793364ba194eff778dc00697bafc3bf2378
abdigantengbanget <abdi.nurhaqqin23@student.uisi.ac.id>
2025-05-12 04:44:45 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,92 @@
+$(document).ready(function() {
+
+	
+	$('.burger-container').on('click', function () {
+    $('.main-navigation').toggle('slow');
+
+    if ($('#myBtn').hasClass('change')) {
+        $('body').addClass('stop-scroll');
+        $('#myBtn').hide(); // Sembunyikan tombol burger
+    } else {
+        $('body').removeClass('stop-scroll');
+        $('#myBtn').show(); // Tampilkan kembali tombol burger
+    }
+});
+
+// Tombol close dari dalam menu
+$('.close-nav').on('click', function () {
+    $('.main-navigation').hide('slow');
+    $('#myBtn').removeClass('change').show(); // Tampilkan kembali tombol burger
+    $('body').removeClass('stop-scroll');
+});
+
+
+	// Close button inside nav
+$('.close-nav').on('click', function() {
+  $('.main-navigation').hide('slow');
+  $('#myBtn').removeClass('change');
+  $('body').removeClass('stop-scroll');
+});
+
+
+	/* About me slider */
+	$('.about-me-slider').slick({
+		slidesToShow: 1,
+		prevArrow: '<span class="span-arrow slick-prev"><</span>',
+		nextArrow: '<span class="span-arrow slick-next">></span>'
+	});
+
+	/* Blog slider */
+	$('.blog-slider').slick({
+		slidesToShow: 2,
+		prevArrow: '<span class="span-arrow slick-prev"><</span>',
+		nextArrow: '<span class="span-arrow slick-next">></span>',
+		responsive: [
+		{
+			breakpoint: 768,
+			settings: {
+				slidesToShow: 1
+			}
+		}
+		]
+	});
+	
+});
+
+
+
+var counta = 0;
+
+$(window).scroll(function(e){
+
+
+	/* Onscroll number counter */
+	var statisticNumbers = $('.single-count');
+	if(statisticNumbers.length) {
+		var oTop = statisticNumbers.offset().top - window.innerHeight;
+		if (counta == 0 && $(window).scrollTop() > oTop) {
+			$('.count').each(function() {
+				var $this = $(this),
+				countTo = $this.attr('data-count');
+				$({
+					countNum: $this.text()
+				}).animate({
+					countNum: countTo
+				},
+
+				{
+					duration: 2000,
+					easing: 'swing',
+					step: function() {
+						$this.text(Math.floor(this.countNum));
+					},
+					complete: function() {
+						$this.text(this.countNum);
+					}
+				});
+			});
+			counta = 1;
+		}
+	}
+
+});
